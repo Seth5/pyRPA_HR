6a91acf29779507b9339a705dc5a11b684ee7d10
sethoh <seth.oh@gmail.com>
2021-02-14 10:01:16 +0900

    Test 3rd commit

---
+++ 26_국민연금.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+
+interval = 2 # 페이지 전환시 2초 대기
+
+# 크롬 브라우저 열기
+browser = webdriver.Chrome()
+
+# 국민연금 사이트로 이동
+browser.get("https://edi.nps.or.kr/")
+time.sleep(interval)
+
